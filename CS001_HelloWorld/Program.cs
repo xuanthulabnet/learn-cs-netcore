87d3f597a3c12640f49e5b3ad79c1c84a17b0e47
XuanThuLab <xuanthulab.net@gmail.com>
2020-08-09 00:20:49 +0700

    CS001 HelloWorld

---
+++ CS001_HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CS001_HelloWorld {
+    class Program // Đây là một ghi chú 1 dòng, đặt ngay sau lệnh C#
+    {
+        static void Main (string[] args) {
+            // Đây là một dòng ghi chú 1 dòng riêng biệt - dòng này không ảnh hưởng đến code
+
+            Console.WriteLine ("Xin chào C# NET CORE!");
+        }
+
+        /// <summary>
+        /// Tính tổng hai số nguyên
+        /// </summary>
+        /// <param name="a">số thứ nhất</param>
+        /// <param name="b">số thứ hai</param>
+        /// <returns>giá trị a + b</returns>
+        static int TongHaiSo (int a, int b) {
+            return a + b;
+        }
+    }
+}
---
+++ CS001_HelloWorld/Readme.md
@@ -0,0 +1,18 @@
+https://xuanthulab.net/gioi-thieu-c-va-viet-chuong-trinh-cs-dau-tien.html
+
+## Create App Console
+```
+dotnet new console
+```
+
+## Create .gitignore for C# - Net Core
+```
+dotnet new gitignore
+```
+
+## Build, Publush, Run
+```
+dotnet buid
+dotnet publish
+dotnet run
+```
