a49d0d58d892aa06f80a8328b3c6ef3a1effa444
XuanThuLab <xuanthulab.net@gmail.com>
2020-08-09 00:33:36 +0700

    CS003 ArithmeticAssignment

---
+++ CS003_ArithmeticAssignment/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace CS003_ArithmeticAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // // Cộng - Trừ
+            // int a = 28;
+            // int b = 10;
+            // Console.WriteLine(a + b);  // In ra: 38
+            // Console.WriteLine(a - b);  // In ra: 18
+
+            // // Nhân
+            // float a = 1.2f;
+            // int b = 3; 
+            // Console.WriteLine(a * b);  // In ra 3.6
+
+            // // Chia
+            // float a = 1.2f;
+            // int b = 3; 
+            // Console.WriteLine(a / b);  // In ra 0.4
+
+            // // Chia 2 số nguyên
+            // int a = 10;
+            // int b = 3; 
+            // Console.WriteLine(a / b);  // In ra 3
+            // Console.WriteLine(a / (float)b);  // In ra 3.333333
+
+            // // Chia lấy dư
+            // int a = 8;
+            // int b = 3; 
+            // Console.WriteLine(a % b);  // In ra 2
+
+            // // Độ ưu tiên
+            // Console.WriteLine(5 + 3 * 2);         // 11
+            // Console.WriteLine(6 / 2 + 3 * 2);     // 9
+            // Console.WriteLine(6 / (2 + 3) * 2);   // 2
+
+            // // Toán tử gán =
+            // int x = 10 + 12;  // x bằng 22
+            // Console.WriteLine(x);
+
+            // // Toán tử += 
+            // int x = 10;
+            // x += 2;         // x bằng 12
+
+            // // Toán tử -=
+            // int c = 10;
+            // c -= 3;         // c = 7
+
+            // //Toán tử *=
+            // int x = 2;
+            // x *= 3;         //x = 6
+
+
+
+            //// Tăng - Giảm
+            // int a = 10;
+            // a++;    // a là 11 (thêm 1)
+            // ++a;    // a là 12 (thêm 1)
+            
+            // a--;    // a là 11 (bớt 1)
+            // --a;    // a là 10 (bớt 1)
+
+            // int a = 5;
+            // int b = 2 * a++;  // b = 10; a = 6 
+
+
+
+
+
+
+
+            
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+## Ví dụ về các toán tử số học (Arithmetic Operators) và toán tử gán (Assignment Operators)
+https://xuanthulab.net/cac-toan-tu-tinh-toan-so-hoc-trong-c-toan-tu-gan-va-tang-giam.html
