ae071ca000b7492d90841abe8adea0421a8122cd
XuanThuLab <xuanthulab.net@gmail.com>
2020-08-10 12:37:55 +0700

    CS005 - for, while, continue, break

---
+++ CS005_for_while/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CS005_for_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // // Vòng lặp for
+            // for (int i = 8;i <=10;i++)
+            // {
+            //     Console.WriteLine("Số i = " + i);
+            // }
+
+            // // Ví dụ lặp với for
+            // int j = 10;
+            // for (;j <=20;) 
+            // {
+            //     Console.WriteLine("Số j = " + j);
+            //     j += 2;
+            // }
+  
+
+            // // Ví dụ vòng lặp while
+            // int i = 8;
+            // while (i <= 10)
+            // {
+            //     Console.WriteLine("Số i = " + i);
+            //     i++;
+            // }
+            // // Số i = 8
+            // // Số i = 9
+            // // Số i = 10
+
+
+            // // Ví dụ continue, in ra số chia hết cho 3
+            // for (int i = 10; i <= 20; i ++ )
+            // {
+            //     if (i % 3 != 0)
+            //         continue;
+            //     Console.WriteLine("Số i = " + i);
+            // }
+            // // Số i = 12
+            // // Số i = 15
+            // // Số i = 18
+
+
+            // Ví dụ ngắt lặp với break;
+            // int i = 0; 
+            // while (true) 
+            // {
+            //     Console.Write(" " + ++i);
+            //     if (i == 10)
+            //         break; // Thoát lặp
+            // }
+            // // 1 2 3 4 5 6 7 8 9 10
+
+
+        }
+    }
+}
