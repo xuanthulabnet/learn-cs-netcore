6bf8417d3cd4b1006fed4ca22af217758fcf6b11
XuanThuLab <xuanthulab.net@gmail.com>
2020-08-20 14:25:58 +0700

    Dipose

---
+++ CS023_Dispose/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CS023_Dispose {
+    class A : IDisposable {
+        bool resource = true;
+        public void Dispose () {
+            Console.WriteLine ("Phương thức này gọi tự động khi hết using");
+            resource = false; // giải phóng tài nguyên
+        }
+    }
+
+
+
+
+    class Program {
+        static void Main (string[] args) {
+
+            // using (var a = new A ()) {
+            //     Console.WriteLine ("Do something ...");
+            // }
+
+        }
+    }
+}
---
+++ CS023_Dispose/WriteData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace CS023_Dispose {
+
+    public class WriteData : IDisposable {
+
+        // trường lưu trạng thái Dispose
+        private bool m_Disposed = false;
+
+        private StreamWriter stream;
+
+        public WriteData (string filename) {
+            stream = new StreamWriter (filename, true);
+        }
+
+        // Phương thức triển khai từ giao diện
+        public void Dispose () {
+            Dispose (true);
+            GC.SuppressFinalize (this);
+        }
+
+        // Nếu disposing = true -> Được thi hành do gọi trực tiếp (do Dispose gọi)
+        // tài nguyên managed, unmanaged được giải phóng
+        // nếu disposing = fale -> Được thi hành bởi phương thức hủy, chỉ cần giải phóng
+        // các toàn nguyên unmanaged.
+        protected virtual void Dispose (bool disposing) {
+            if (!m_Disposed) {
+                if (disposing) {
+                    // các đối tượng có Dispose gọi ở đây
+                    stream.Dispose();
+                }
+
+                // giải phóng các tài nguyên không quản lý được cửa lớp (unmanaged)
+
+                m_Disposed = true;
+            }
+        }
+
+        ~WriteData () {
+            Dispose(false);
+        }
+
+    }
+
+}
