b04b21405050d08bebdfde8693880e0c99f67272
XuanThuLab <xuanthulab.net@gmail.com>
2020-08-21 15:48:42 +0700

    HttpClient Example

---
+++ CS029_Networking/1.HttpClientExample/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace HttpClientExample
+{
+    
+    class Program
+    {
+        /// In ra thông tin các Header của HTTP Response
+        public static void ShowHeaders(HttpHeaders headers) 
+        {
+            Console.WriteLine("Các Header:");
+            foreach (var header in headers) 
+            {
+                string value = string.Join(" ", header.Value);              
+                Console.WriteLine($"{header.Key,20} : {value}"); 
+             }
+            Console.WriteLine(); 
+         }
+
+        // Tải về và hiện thị thông tin trang tải về, 
+        // url là địa chỉ cần tải ví dụ: https://google.com.vn 
+        public static async Task<string> GetWebContent(string url) 
+        {
+            using (var httpClient = new  HttpClient())
+            {
+                Console.WriteLine($"Starting connect {url}");
+                try {
+                    // Thêm header vào HTTP Request
+                    httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml+json");
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+                    
+                    // Phát sinh Exception nếu mã trạng thái trả về là lỗi 
+                    response.EnsureSuccessStatusCode();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Tải thành công - statusCode {(int)response.StatusCode} {response.ReasonPhrase}");
+                        // Đọc thông tin header trả về
+                        ShowHeaders(response.Headers);
+                        
+
+                        Console.WriteLine("Starting read data");
+
+                        // Đọc nội dung content trả về
+                        string htmltext = await response.Content.ReadAsStringAsync(); 
+                        Console.WriteLine($"Nhận được {htmltext.Length} ký tự"); 
+                        Console.WriteLine();
+                        return htmltext;
+                    }
+                    else 
+                    {
+                        Console.WriteLine($"Lỗi - statusCode {response.StatusCode} {response.ReasonPhrase}");
+                        return null;
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var htmltask = GetWebContent("https://google.com.vn");
+            htmltask.Wait(); // Chờ tải xong
+            // Hoặc wait htmltask; nếu chuyển Main thành async 
+           
+            var html = htmltask.Result;
+            Console.WriteLine(html!=null ? html.Substring(0, 255): "Lỗi"); 
+        }
+    }
+}
---
+++ CS029_Networking/readme.md
@@ -0,0 +1,5 @@
+# Ví dụ về Networking trong C#
+https://xuanthulab.net/networking-su-dung-httpclient-trong-c-tao-cac-truy-van-http.html
+https://xuanthulab.net/networking-su-dung-httpmessagehandler-cho-httpclient-trong-c-csharp.html
+https://xuanthulab.net/networking-su-dung-lop-httplistener-trong-c-de-tao-may-chu-web-http-don-gian.html
+https://xuanthulab.net/networking-giao-thuc-tcp-voi-cac-lop-tcplistener-tcpclient-va-cac-lop-uri-ipaddress-c-c-sharp.html
