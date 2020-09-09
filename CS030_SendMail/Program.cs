7c16d51ae688e48c22bfd4d891a02ee861d9cdc6
XuanThuLab <xuanthulab.net@gmail.com>
2020-09-09 16:36:59 +0700

    SendMail

---
+++ CS030_SendMail/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using MailUtils;
+
+namespace CS030_SendMail {
+    class Program {
+
+        static void Main (string[] args) {
+
+            String mailnhan = "youremail.abc@gmail.com";
+            String mailgui = "yoremail.xyz@gmail.com";
+            String chude = "Kiểm tra email gửi đi";
+            String noidung = @"<h1>Xin chào XuanThuLab</h1><p>Đây là mail gửi sử dụng gmail</p>";
+
+            string smtpacount = "yourmailacount@gmail.com";
+            string smtppassword = "PASSWORDCUABAN";
+
+            
+            MailUtils.MailUtils.SendMailGoogleSmtp(
+                mailgui,
+                mailnhan,
+                chude,
+                noidung,
+                smtpacount,
+                smtppassword
+
+            ).Wait();
+
+
+            // Nếu hàm main là asyn chì gọi
+            // await MailUtils.MailUtils.SendMailGoogleSmtp(...);
+        }
+    }
+}
