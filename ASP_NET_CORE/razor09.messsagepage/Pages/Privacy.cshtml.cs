7a27789d22c5ba984aab21d587421be341e7a399
XuanThuLab <xuanthulab.net@gmail.com>
2020-09-10 11:34:11 +0700

    Redirect Message Page

---
+++ ASP_NET_CORE/razor09.messsagepage/Pages/Privacy.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace razor09.messsagepage.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            return ViewComponent("MessagePage", new XTLASPNET.MessagePage.Message {
+                title = "Thông báo quan trọng",
+                htmlcontent = "Đây là <strong>Nội dung HTML</strong>",
+                secondwait = 5,
+                urlredirect = "/"
+            });
+        }
+    }
+}
