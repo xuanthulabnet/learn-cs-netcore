978d6fe1c093762f4bf7c498c08a0cce3064d498
XuanThuLab <xuanthulab.net@gmail.com>
2020-09-12 15:31:43 +0700

    Identity 3 - Google Login

--- ASP_NET_CORE/Album/Startup.cs
+++ ASP_NET_CORE/Album/Startup.cs
@@ -42,10 +42,29 @@ namespace Album {
                 options.UseSqlServer (connectstring);
             });
 
+
+
             // Đăng ký Identity
             services.AddIdentity<AppUser, IdentityRole> ()
                 .AddEntityFrameworkStores<AppDbContext> ()
                 .AddDefaultTokenProviders ();
+
+            services.AddAuthentication()
+                .AddGoogle(googleOptions =>
+                {
+                    // Đọc thông tin Authentication:Google từ appsettings.json
+                    IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
+
+                    // Thiết lập ClientID và ClientSecret để truy cập API google
+                    googleOptions.ClientId = googleAuthNSection["ClientId"];
+                    googleOptions.ClientSecret = googleAuthNSection["ClientSecret"];
+                    // Cấu hình Url callback lại từ Google (không thiết lập thì mặc định là /signin-google)
+                    googleOptions.CallbackPath = "/dang-nhap-tu-google";
+                    
+                });
+               
+
+     
 
             // Truy cập IdentityOptions
             services.Configure<IdentityOptions> (options => {
