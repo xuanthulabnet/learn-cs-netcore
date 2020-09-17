393cca2e3be377af6035893548aeeadf90156d7e
XuanThuLab <xuanthulab.net@gmail.com>
2020-09-17 08:15:57 +0700

    Authorize

---
+++ ASP_NET_CORE/Album/Pages/TestAuthorize1.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Album.Pages
+{
+    [Authorize(Roles = "Admin")]
+    public class TestAuthorize1Model : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+        public void OnGetAbc() {
+            
+        }
+    }
+}
