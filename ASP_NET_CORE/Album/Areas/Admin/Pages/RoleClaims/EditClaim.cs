e4918461516843c8630761a2c92bedee50cc9e68
XuanThuLab <xuanthulab.net@gmail.com>
2020-09-18 08:18:55 +0700

    Role-base claim

---
+++ ASP_NET_CORE/Album/Areas/Admin/Pages/RoleClaims/EditClaim.cs
@@ -0,0 +1,9 @@
+namespace Album.Areas.Admin.Pages.RoleClaims
+{
+        public class EditClaim {
+            public int Id {set; get;}
+            public string ClaimType {set; get;}
+            public string ClaimValue  {set; get;}
+        }
+
+}
